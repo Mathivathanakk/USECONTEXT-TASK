7674da1df9c2820b104d708320a82679e4b2e36e
Mathivathanakk <mathikk06@gmail.com>
2024-05-14 19:44:17 +0530

    updated

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -68,82 +68,117 @@ const Cart = () => {
                 <div className="row">
                   <div className="col-md-4">
                     {/* image */}
-                    
-                       
-                          <img
-                            src={element.images[0]}
-                            className="card-img-top"
-                            alt="phone-image"
-                          />
-                    
-                    {/* remove button */}
-                    <button
-                      onClick={() => removeCart(element.id)}
-                      className="btn btn-danger m-4"
-                    >
-                      REMOVE
-                    </button>
+
+                    <div>
+                      <img
+                        src={element.images[0]}
+                        className="card-img-top"
+                        alt="phone-image"
+                      />
+                    </div>
                   </div>
                   {/* start card body */}
-                  <div className="col-md-8">
+                  <div className="col-md-4">
                     <div className="card-body ">
-                      <h2 className="card-title text-center">
-                        {element.title}
-                      </h2>
-                      <h6 className="card-subTitle text-center">
+                      <h2 className="card-title d-flex">{element.title}</h2>
+                      <h6
+                        className="card-subTitle d-flex fs-4 "
+                        style={{ color: "rgb(68, 66, 66)" }}
+                      >
                         Category :{element.category}
                       </h6>
-                      <p className="card-text text-center">
+                      <p
+                        className="card-text d-flex fs-5"
+                        style={{ color: "rgb(68, 66, 66)" }}
+                      >
                         {element.description}
                       </p>
-                      {/* button increment and decrement */}
-                      <div className="text-center">
-                        <button
-                          onClick={() =>
-                            handleInc(element.id, element.quantity || 1)
-                          }
-                          className="btn btn-primary "
-                        >
-                          +
-                        </button>
-                        <span className="fw-bold fs-5 p-4">
-                          {element.quantity || 1}
-                        </span>
-                        <button
-                          onClick={() =>
-                            handleDec(element.id, element.quantity || 1)
-                          }
-                          className="btn btn-primary "
-                        >
-                          -
-                        </button>
-                      </div>
-                      {/* price */}
-                      <div className="mt-3 text-center">
-                        <h3>
-                          Price:<span className="fw-bold">{element.price}</span>{" "}
-                        </h3>
-                      </div>
-                      {/* Shipping */}
-                      <div
-                        className="text fw-bold  m-2 fs-4 text-center"
-                        style={{ color: "gray" }}
-                      >
-                        Shipping: FREE
-                      </div>
-                      {/* subtotal */}
-                      <div className="text-center">
-                        <h3>
-                          Subtotal:
-                          <span className="fw-bold fs-2">
-                            {element.price * element.quantity || element.price}
+                    </div>
+                  </div>
+                  {/* button increment and decrement */}
+                  <div className="col-md-4">
+                    <div className="card-body">
+                      <div className="d-flex justify-content-between align-items-center ">
+                        <div>
+                          <button
+                            onClick={() =>
+                              handleInc(element.id, element.quantity || 1)
+                            }
+                            className="btn btn-warning "
+                            style={{ color: "white" }}
+                          >
+                            +
+                          </button>
+                          <span className="fw-bold fs-5 p-3">
+                            {element.quantity || 1}
                           </span>
-                        </h3>
+                          <button
+                            onClick={() =>
+                              handleDec(element.id, element.quantity || 1)
+                            }
+                            className="btn btn-warning fw-bold "
+                            style={{ color: "white" }}
+                          >
+                            -
+                          </button>
+                        </div>
+                        {/* price */}
+
+                        <h2>${element.price}</h2>
+                      </div>
+                      {/* remove button */}
+                      <div className="d-flex justify-content-end  align-content-end mt-5">
+                        <button
+                          onClick={() => removeCart(element.id)}
+                          className="btn btn-warning  m-4"
+                          style={{ color: "white" }}
+                        >
+                          REMOVE
+                        </button>
                       </div>
                     </div>
-                    {/* end of card body */}
                   </div>
                 </div>
+                <hr />
+                <div className="row">
+                  <div className="col">
+                    {/* subtotal */}
+                    <div className="d-flex justify-content-between align-items-center">
+                      <h3 style={{ color: "rgb(68, 66, 66)" }}>Subtotal</h3>
+                      <h3 className="fw-bold fs-2">
+                        ${element.price * element.quantity || element.price}
+                      </h3>
+                    </div>
+                    {/* Shipping */}
+                    <div
+                      className="text fw-bold  m-2 fs-4 d-flex justify-content-between align-items-center"
+                      style={{ color: "black" }}
+                    >
+                      <h4 style={{ color: "rgb(68, 66, 66)" }}>Shipping</h4>
+                      <h4>FREE</h4>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col">
+                    <div className="text d-flex justify-content-between align-items-center  ">
+                      <h3 style={{ color: "black" }}>Total Price</h3>
+                      <h3
+                        className="fw-bold fs-1"
+                        style={{
+                          color: "black",
+                          padding: "5px",
+                          borderRadius: "10px",
+                          margin: "10px",
+                        }}
+                      >
+                        $ {totalPrice}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                {/* end of card body */}
               </div>
             </div>
           );
@@ -154,37 +189,36 @@ const Cart = () => {
             {/* start of card */}
             <div className="card mt-4 p-4">
               {/* total quantity */}
-
-              <h3 className="text-center" style={{ color: "black" }}>
-                Total Quantity:
-                <span
+              <div className="d-flex justify-content-between align-items-center ">
+                <h3 style={{ color: "black" }}>Total Quantity</h3>
+                <h3
+                  className="fw-bold fs-1"
                   style={{
-                    backgroundColor: "rgb(61,7,71)",
-                    color: "white",
+                    color: "black",
                     padding: "5px",
                     borderRadius: "10px",
                     margin: "10px",
                   }}
                 >
                   {totalQuantity}
-                </span>
-              </h3>
+                </h3>
+              </div>
               <br />
               {/* total price */}
-              <h3 className="text-center" style={{ color: "black" }}>
-                Total Price:
-                <span
+              <div className="text d-flex justify-content-between align-align-items-center  ">
+                <h3 style={{ color: "black" }}>Total Price</h3>
+                <h3
+                  className="fw-bold fs-1"
                   style={{
-                    backgroundColor: "rgb(61,7,71)",
-                    color: "white",
+                    color: "black",
                     padding: "5px",
                     borderRadius: "10px",
                     margin: "10px",
                   }}
                 >
-                  {totalPrice}
-                </span>
-              </h3>
+                  $ {totalPrice}
+                </h3>
+              </div>
             </div>
             {/* end of the card */}
           </div>
